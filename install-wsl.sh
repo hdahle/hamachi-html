5f8955827f14f325f04e6f6525e8997824b12354
Håkon Dahle <hakon.dahle@gmail.com>
2021-02-15 11:03:22 +0100

    install-wsl now checks directory exists

--- install-wsl.sh
+++ install-wsl.sh
@@ -3,8 +3,10 @@
 # move files to WSL/Ubuntu Apache HTML folder
 
 sudo cp index.html /var/www/html/
-sudo cp js/* /var/www/html/js/
-sudo cp css/*.css /var/www/html/css/
+
+[ -d js ] && sudo cp js/* /var/www/html/js/
+[ -d css ] && sudo cp css/* /var/www/html/css/
+[ -d img ] && sudo cp img/* /var/www/html/img/
+
 sudo cp favicon.ico /var/www/html
-sudo cp img/* /var/www/html/img/
 
