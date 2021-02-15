277294044b4477753180a58fafa1e90bfbdbd8b4
Håkon Dahle <hakon.dahle@gmail.com>
2021-02-15 10:21:07 +0100

    New logo

--- install-wsl.sh
+++ install-wsl.sh
@@ -3,11 +3,8 @@
 # move files to WSL/Ubuntu Apache HTML folder
 
 sudo cp index.html /var/www/html/
-sudo cp js/app.js /var/www/html/js/
-sudo cp js/dom.js /var/www/html/js/
-sudo cp js/redis-values.js /var/www/html/js/
+sudo cp js/* /var/www/html/js/
 sudo cp css/*.css /var/www/html/css/
 sudo cp favicon.ico /var/www/html
-sudo cp eco-mark.svg /var/www/html
 sudo cp img/* /var/www/html/img/
 
